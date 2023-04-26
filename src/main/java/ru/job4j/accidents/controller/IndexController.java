74cb8c1eaadab015430f2f30948e455975f99443
Igor <swebdesigne@gmail.com>
2023-04-26 10:01:09 +0300

    Spring MVC

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -2,12 +2,13 @@ package ru.job4j.accidents.controller;
 
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @ThreadSafe
 @Controller
 public class IndexController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "index";
     }
